91b5b8afcdc396db6a963f4a54062627b60fda27
timmitommi <timaslj@gmail.com>
2017-05-09 13:45:58 +0200

    Preliminary movement implemented
    bad programming

--- src/code/Bird.java
+++ src/code/Bird.java
@@ -1,7 +1,59 @@
 package code;
 
+
+import javafx.animation.RotateTransition;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
 /**
  * Created by Kristoffer on 2017-05-08.
  */
 public class Bird {
+    protected ImageView bird;
+    protected Image birdImage;
+    protected double startingX;
+    protected double startingY;
+    protected double currentY;
+    protected double speedY;
+    private double gravity;
+    protected boolean falling;
+    private double maxSpeedY;
+
+    public Bird(){
+        birdImage = new Image("/assets/pictures/bird.png", 0, 120, true,true,true);
+        bird = new ImageView(birdImage);
+
+        startingY = Game.SCREEN_HEIGHT/6;
+        startingX = Game.SCREEN_HEIGHT/6;
+        currentY = startingY;
+        gravity = 1;
+        falling = true;
+        maxSpeedY = 15;
+
+    }
+
+    public void fall(){
+        if (falling){
+            speedY += gravity;
+            if (speedY > maxSpeedY){
+                speedY = maxSpeedY;
+            }
+            currentY += speedY;
+
+        }
+    }
+
+    public void jump(){
+        if (!falling){
+            speedY -= gravity;
+            if(speedY < maxSpeedY){
+                speedY = maxSpeedY;
+            }
+
+            currentY -= speedY;
+        }
+    }
+
+
 }
--- src/code/Game.java
+++ src/code/Game.java
@@ -1,8 +1,17 @@
 package code;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.RotateTransition;
+import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by Kristoffer on 2017-05-08.
@@ -13,10 +22,16 @@ public class Game {
     private View view;
     private Background background;
     private Ground ground;
+    private Bird bird;
     private double backgroundScrollSpeed = 0.4;
     private double groundScrollSpeed = 5;
     public static double SCREEN_WIDTH = Screen.getPrimary().getVisualBounds().getWidth();
     public static double SCREEN_HEIGHT = Screen.getPrimary().getVisualBounds().getHeight();
+    private Canvas canvas;
+    private GraphicsContext gc;
+    ArrayList<String> input;
+    private RotateTransition rotateTransition;
+
 
     public Game(Stage primaryStage) {
         init(primaryStage);
@@ -27,19 +42,64 @@ public class Game {
         view = new View();
         background = new Background(SCREEN_HEIGHT, SCREEN_WIDTH);
         ground = new Ground();
+        bird = new Bird();
 
         view.createView(primaryStage);
         view.addNode(background);
         view.addNode(ground);
+
+        canvas = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        gc = canvas.getGraphicsContext2D();
+
+        view.addNode(canvas);
+
+        input = new ArrayList<String>();
+
+        view.scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                String code = event.getCode().toString();
+
+                if(!input.contains(code)) {
+                    input.add(code);
+                }
+            }
+        });
+
+        view.scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                String code = event.getCode().toString();
+                input.remove(code);
+            }
+        });
     }
 
     private void startGameLoop() {
 
         gameLoop = new AnimationTimer() {
             @Override
-            public void handle(long l) {
+            public void handle(long currentNanoTime) {
+                gc.clearRect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
+
                 background.scrollBackground(backgroundScrollSpeed);
                 ground.scrollGround(groundScrollSpeed);
+
+                if (input.contains("SPACE")) {
+                    bird.speedY = 0;
+                    bird.falling = false;
+                    bird.jump();
+                    gc.drawImage(bird.birdImage, bird.startingX, bird.currentY);
+                }
+                else {
+                    if(bird.falling == false){
+                        bird.speedY = 0;
+                        bird.falling = true;
+                    }
+                    bird.fall();
+                    gc.drawImage(bird.birdImage, bird.startingX, bird.currentY );
+                }
             }
         };
         gameLoop.start();
